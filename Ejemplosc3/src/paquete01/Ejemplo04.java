0ef2c93037927b07de025f2f63426d2dc1e21620
unknown <luiseduardosa2006@gmail.com>
2024-12-18 11:57:38 -0500

    xd

--- Ejemplosc3/src/paquete01/Ejemplo04.java
+++ Ejemplosc3/src/paquete01/Ejemplo04.java
@@ -18,6 +18,7 @@ public class Ejemplo04 {
         // Creación de arreglo bidimensionales
         
         // forma 1
+        int sumaTotal=0;
         int[][] arreglo1 = {
             
             {10, 20, 40},  // fila 0
@@ -39,7 +40,9 @@ public class Ejemplo04 {
                      // 30 + arreglo1[0][2] // 30 + 40 // 70
             }
             System.out.printf("Suma de valores del arreglo: %d\n", suma);
+            sumaTotal= sumaTotal+suma;
         }
+        System.out.println("Suma total: "+ sumaTotal);
         
         // System.out.printf("Suma de valores del arreglo: %d\n", suma);
     }
